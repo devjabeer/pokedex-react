192b7bb6f4bd6c9b1619a61428fc531bdb3ee586
Jabeer <mdjabeer1994@gmail.com>
2022-04-06 17:32:06 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Pokedex.jsx
@@ -0,0 +1,8 @@
+
+function Pokedex() {
+  return (
+    <div><h1>Pkedex</h1></div>
+  )
+}
+
+export default Pokedex
